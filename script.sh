f07e9877b548b499470897e2abe36d8e4dec238e
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-21 18:13:31 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+    $ nano (+nombre del archivo).
+    $ nano /data/data/com.termux/files/usr/etc/bash.bashrc
+
+// Enter después del parámmetro: fi.
+// Sacar la ruta del banner con pwd..
+
+    $ cat  /data/data/com.termux/files/home/T-banner/Skull  |  lolcat
+
+// Para apariencia de color lolcat:
+
+    $ pkg install ruby -y
+    $ gem install lolcat
+    
